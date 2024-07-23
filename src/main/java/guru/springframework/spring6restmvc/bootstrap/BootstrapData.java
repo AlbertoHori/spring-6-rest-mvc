0ac3d3f7e97b547686dc48fcee95796d1ca6c3b5
albertohori <vanessa._u._alberto-hori@t-online.de>
2024-07-23 08:35:57 +0200

    assignment 3

---
+++ src/main/java/guru/springframework/spring6restmvc/bootstrap/BootstrapData.java
@@ -0,0 +1,60 @@
+package guru.springframework.spring6restmvc.bootstrap;
+
+import guru.springframework.spring6restmvc.entities.Beer;
+import guru.springframework.spring6restmvc.entities.Customer;
+import guru.springframework.spring6restmvc.model.BeerStyle;
+import guru.springframework.spring6restmvc.repositories.BeerRepository;
+import guru.springframework.spring6restmvc.repositories.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+/**
+ * FECIT AH DIE:22.07.24
+ */
+@Component
+@RequiredArgsConstructor
+public class BootstrapData implements CommandLineRunner {
+
+    private final BeerRepository beerRepository;
+    private final CustomerRepository customerRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Beer beer1 = Beer.builder()
+                .beerName("Galaxy Cat")
+                .beerStyle(BeerStyle.PALE_ALE)
+                .upc("12356")
+                .price(new BigDecimal("12.99"))
+                .quantityOnHand(122)
+                .createdDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .build();
+
+        Beer beer2 = Beer.builder()
+                .beerName("Flensburger")
+                .beerStyle(BeerStyle.PALE_ALE)
+                .upc("12356")
+                .price(new BigDecimal("12.99"))
+                .quantityOnHand(122)
+                .createdDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .build();
+
+
+
+        Customer customer1 = Customer.builder()
+                .name("Pumpin")
+                .build();
+
+
+        beerRepository.save(beer1);
+        beerRepository.save(beer2);
+        customerRepository.save(customer1);
+
+    }
+}
